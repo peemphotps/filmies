71f395985757f99ed201f4425cc765a44b289942
Peemphot <premphot@gmail.com>
2026-06-07 12:15:05 +0700

    Initial Commit

---
+++ Filmies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Filmies
+//
+//  Created by Warunkan Konnantaphat on 7/6/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Filmies/FilmiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmiesApp.swift
+//  Filmies
+//
+//  Created by Warunkan Konnantaphat on 7/6/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct FilmiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
